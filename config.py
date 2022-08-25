2b8e302f99d49037d84e447a3a00142442e8fe08
affdlh <36109810+affdlh@users.noreply.github.com>
2022-08-25 14:31:44 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# MENGGUNAKAN CAPTCHA : anycaptcha / anti-captcha
+CAPTCHA_PROVIDER = "anti-captcha"
+# ANTI CAPTCHA KEY
+ANTI_CAPTCHA_KEY = "xxxx"
+# ANY CAPTCHA KE
+ANY_CAPTCHA_KEY = "xxxx"
+# COIN
+COIN = "trx"
+COUNTDOWN = 5 # countdown / waktu mundur / jeda claim / satuan detik
+PROVIDER = "stake.com" # LINK STAKE
